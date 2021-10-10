1cfb76a7d7ca5fc495f1eacdac3f37b3a6751719
activationeuron <aforamitrai@gmail.com>
2021-10-10 23:55:14 +0530

    added files

---
+++ build/precache-manifest.6e6c5d02be419912e2d74b83e3680e6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900d6b185e03ab9062d969c643757022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e77f2f3e249018c4e3c0",
+    "url": "/static/css/2.7d2606b6.chunk.css"
+  },
+  {
+    "revision": "2bbad646ca662b898ffd",
+    "url": "/static/css/main.69b20f1e.chunk.css"
+  },
+  {
+    "revision": "e77f2f3e249018c4e3c0",
+    "url": "/static/js/2.ac1fafb6.chunk.js"
+  },
+  {
+    "revision": "e1fe29bd6814ff31f8c1a7a79ef3be10",
+    "url": "/static/js/2.ac1fafb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbad646ca662b898ffd",
+    "url": "/static/js/main.9a707b43.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
